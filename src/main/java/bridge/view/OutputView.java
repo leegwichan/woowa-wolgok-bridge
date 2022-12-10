9a7843b92890bcd54bccac1bffbf16c8e650ac10
LeeHaiLim <hailim1997@gmail.com>
2022-12-10 15:00:55 +0900

    feat(view): implement method print result
    Added methods:
    - print success of fail
    - print attempt

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -9,6 +9,24 @@ public class OutputView {
         System.out.println(map);
     }
 
-    public void printResult() {
+    public void printResult(String map, int attempt, boolean isFail) {
+        System.out.println("최종 게임 결과");
+        printMap(map);
+        printSuccessOrFail(isFail);
+        printAttempt(attempt);
+    }
+
+    private void printSuccessOrFail(boolean isFail) {
+        String format = "게임 성공 여부: %d\n" ;
+        String result = "성공" ;
+        if (isFail) {
+            result = "실패" ;
+        }
+        System.out.printf(format, result);
+    }
+
+    private void printAttempt(int attempt) {
+        String format = "총 시도한 횟수: %d\n" ;
+        System.out.printf(format, attempt);
     }
 }
