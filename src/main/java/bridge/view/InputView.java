925883301fa7bc70eaa8572b65967b8a1458e3f0
EUNJEONGMUN <munej26@gmail.com>
2022-12-10 15:22:46 +0900

    fix: 정적 메서드로 수정

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -19,7 +19,7 @@ public class InputView {
         return Console.readLine();
     }
 
-    public String readGameCommand() {
+    public static String readGameCommand() {
         System.out.println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)");
         return Console.readLine();
     }
--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -6,7 +6,7 @@ package bridge.view;
 public class OutputView {
     private static final String PREFIX_ERROR_MESSAGE = "[ERROR] ";
 
-    public void printMap(String bridgeMap) {
+    public static void printMap(String bridgeMap) {
         System.out.println(bridgeMap);
     }
 
@@ -15,7 +15,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public static void printResult() {
     }
 
     public static void printErrorMessage(String message) {
