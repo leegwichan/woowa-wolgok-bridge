6e1dc51a7a7b3110c7137b0d65086e2f879bddb4
EUNJEONGMUN <munej26@gmail.com>
2022-12-10 16:39:15 +0900

    test: 다리 건너기 테스트

---
+++ src/test/java/bridge/domain/BridgeTest.java
@@ -0,0 +1,35 @@
+package bridge.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BridgeTest {
+    private Bridge bridge;
+    @BeforeEach
+    void setUp() {
+        bridge = Bridge.from(List.of("U", "D"));
+    }
+
+    @DisplayName("다리 건너기 성공 테스트")
+    @Test
+    void createSuccessMove() {
+        Player player = new Player();
+        Direction firstMove = Direction.from("U");
+        Assertions.assertThat(bridge.canMove(player, firstMove)).isTrue();
+    }
+
+    @DisplayName("다리 건너기 실패 테스트")
+    @Test
+    void createWrongMove() {
+        Player player = new Player();
+        Direction firstMove = Direction.from("D");
+        Assertions.assertThat(bridge.canMove(player, firstMove)).isFalse();
+    }
+}
