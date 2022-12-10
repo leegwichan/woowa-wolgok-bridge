557484a16dc4176123ebcb6f7b2315cf8f9bc551
EUNJEONGMUN <munej26@gmail.com>
2022-12-10 13:01:57 +0900

    feat: Floor객체 및 Bridge 객체 생성

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,4 +1,21 @@
 package bridge.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Bridge {
+
+    private final List<Floor> bridge;
+
+    private Bridge(List<String> bridge) {
+        this.bridge = bridge.stream()
+                .map(Floor::from)
+                .collect(Collectors.toUnmodifiableList());
+
+    }
+
+    public static Bridge from(List<String> bridge) {
+        return new Bridge(bridge);
+    }
 }
--- src/main/java/bridge/domain/Floor.java
+++ src/main/java/bridge/domain/Floor.java
@@ -1,4 +1,31 @@
 package bridge.domain;
 
+import java.util.Objects;
+
 public class Floor {
+    private final String direction;
+
+    private Floor(String direction) {
+        this.direction = direction;
+    }
+
+    public static Floor from(String direction) {
+        return new Floor(direction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(direction);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Floor)) {
+            return false;
+        }
+        if (((Floor) obj).direction.equals(this.direction)) {
+            return true;
+        }
+        return false;
+    }
 }
