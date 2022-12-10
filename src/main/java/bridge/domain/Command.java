445de60fc27420b6bfbf84a0ceace4cd69297409
EUNJEONGMUN <munej26@gmail.com>
2022-12-10 12:37:56 +0900

    feat: 게임 재시작여부 입력 기능 구현

--- src/main/java/bridge/domain/Command.java
+++ src/main/java/bridge/domain/Command.java
@@ -1,5 +1,20 @@
 package bridge.domain;
 
+import java.util.Arrays;
+
 public enum Command {
-    R, Q
+    RESTART("R"),
+    QUIT("Q");
+
+    private final String mark;
+
+    Command(String mark) {
+        this.mark = mark;
+    }
+
+    public static Command from(String mark) {
+        return Arrays.stream(Command.values()).filter(command -> command.mark.equals(mark))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("R(재시작) 또는 Q(종료)를 입력해주세요."));
+    }
 }
