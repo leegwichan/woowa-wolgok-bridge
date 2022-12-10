1df075e3b7c7205bad9926c081a95bd1bb4d231a
EUNJEONGMUN <munej26@gmail.com>
2022-12-10 16:26:42 +0900

    test: Direction 예외처리 테스트

--- src/test/java/bridge/domain/DirectionTest.java
+++ src/test/java/bridge/domain/DirectionTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DirectionTest {
 
     @DisplayName("0을 입력하면 D, 1을 입력하면 U를 반환한다.")
@@ -15,6 +13,23 @@ class DirectionTest {
     @CsvSource(value = {"0:D", "1:U"}, delimiter = ':')
     void findMarkByCode(int code, String expected) {
         String result = Direction.findMarkByCode(code);
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("U 또는 D가 아닐 경우 예외처리한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"R", "Q", "ABC"})
+    void createInvalidCommand(String input) {
+        Assertions.assertThatThrownBy(() -> Direction.from(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("올바른 이동 방향을 입력해주세요.");
+    }
+
+    @DisplayName("U 또는 D를 입력할 경우 정상적으로 Direction 객체를 생성한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"U:UP", "D:DOWN"}, delimiter = ':')
+    void createValidCommand(String input, Direction expected) {
+        Direction result = Direction.from(input);
         Assertions.assertThat(result).isEqualTo(expected);
     }
 }
