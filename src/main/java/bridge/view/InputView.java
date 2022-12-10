48b06025cdeafbdda75a0e37ddb2a7b081639bd0
LeeHaiLim <hailim1997@gmail.com>
2022-12-10 16:50:55 +0900

    feat(view): add missing input message

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -5,7 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public int readBridgeSize() {
-        System.out.println("다리의 길이를 입력해주세요.");
+        System.out.println("다리 건너기 게임을 시작합니다.\n"
+                + "\n다리의 길이를 입력해주세요.");
         try {
             return Integer.parseInt(Console.readLine().trim());
         } catch (NumberFormatException e) {
